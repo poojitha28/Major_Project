75734b9489c5b686dcd02cbfb1dd9ad3d6911eb7
poojitha <poojitha.maganti@gmail.com>
2015-12-28 14:46:53 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
-  mount Rapidfire::Engine => "/rapidfire"
+  mount Rapidfire::Engine => "/course"
   root to: "survey#home"
   get '/program' => "survey#program"
 end
